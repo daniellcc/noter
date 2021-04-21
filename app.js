2ec9af1321761cd5d046d051de47fb20a00d4562
daniellcc <daniaelcc@gmail.com>
2021-04-21 01:07:06 -0430

    fix

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('view engine', 'ejs')
 
 // middlewares
 app.use(express.static(path.join(__dirname, 'public')))
+app.use('*', express.static(path.join(__dirname, 'public')))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(compression())
