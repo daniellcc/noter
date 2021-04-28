723b9f70ce10b70825a24722ae99299ba65dbb95
daniellcc <daniaelcc@gmail.com>
2021-04-28 00:58:10 -0430

    env variables for db in prod and dev

--- config/db.js
+++ config/db.js
@@ -1,11 +1,26 @@
 const mysql = require('mysql')
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'app',
-  connectionLimit: 99
-})
+let dbConfig
+
+if(process.env.NODE_ENV == 'development') {
+  dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'app',
+    connectionLimit: 100
+  }
+}
+else {
+  dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    connectionLimit: 100
+  } 
+}
+
+const pool = mysql.createPool(dbConfig)
 
 module.exports = pool
